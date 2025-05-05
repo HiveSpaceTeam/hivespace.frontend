8e5b229793017082a0f15312d16067dc9fa3d27e
Lelouch <phuctinh2001@gmail.com>
2025-05-05 22:57:35 +0700

    Handle errorcode

---
+++ src/i18n/vi/i18nErrorCode.js
@@ -0,0 +1,8 @@
+export default {
+    name: "vi",
+    phoneNumberExisted: "Số điện thoại đã tồn tại",
+    invalidSku: "Sku không hợp lệ",
+    invalidMoney: "Số tiền không hợp lệ",
+    invalidPhoneNumber: "Số điện thoại không hợp lệ",
+    incorrectPassword: "Mật khẩu không chính xác"
+};
