457de309a7a7983a1ccafc0a470cadd63f1d9d7f
thangnv-0703 <sunshines0703@gmail.com>
2025-06-11 21:34:44 +0700

    Fix: Declare 'user' variable in oAuthSignin function for proper scoping

--- src/auth/oauth-helper.js
+++ src/auth/oauth-helper.js
@@ -4,7 +4,7 @@ import store from "@/stores/store";
 const oAuthSignin = async () => {
   store.commit("moduleLoading/setLoading", true, { root: true });
   try {
-    user = await userManager.signinSilent();
+    const user = await userManager.signinSilent();
     console.log("Silent signin successful", user);
   } catch (error) {
     console.warn("Silent signin failed, redirecting to login", error);
